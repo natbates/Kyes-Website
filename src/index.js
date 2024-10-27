bc7303169b5007aae1e92f29742c61985457f874
grapes1111 <kyebm@outlook.com>
2024-10-27 22:23:03 +0000

    upload env and edit .gitignore

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import React from "react";
 import App from "./comps/App";
+import "dotenv";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
