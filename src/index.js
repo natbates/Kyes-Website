65af017d8e6d6857bd214362d126cb1d2a375a10
grapes1111 <kyebm@outlook.com>
2024-10-27 22:24:39 +0000

    remove import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client";
 import React from "react";
 import App from "./comps/App";
-import "dotenv";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
