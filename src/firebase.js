0c702f2d675f01b52d2a4f648be20f1944e84d34
Grapes1111 <kyebm@outlook.com>
2025-01-24 00:35:10 +0000

    New commit to add new code

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,13 @@ import { getAnalytics } from 'firebase/analytics';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET, 
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCJjuw-bbZv1h8KIylIkFiSu5sPfxmZt4g",
+  authDomain: "images-76f3b.firebaseapp.com",
+  projectId: "images-76f3b",
+  storageBucket: "images-76f3b.firebasestorage.app",
+  messagingSenderId: "906203051920",
+  appId: "1:906203051920:web:5c4fe47ee0866449bb2515",
+  measurementId: "G-7ETFTSWL2G"
 };
 
 // Initialize Firebase
